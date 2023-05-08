dd237bd3084060b0ab84ebb375337ac2ff428ab7
Your Name <you@example.com>
2023-05-08 17:51:56 +0200

    migration

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,7 +11,7 @@
 <body>
     <div class="container">
         <h1>Template Classe 89</h1>
-        <img src="{{ Vite::asset('resources/img/pluto.webp') }}" alt="" srcset="">
+        @dd($trains);
     </div>
 
 </body>
